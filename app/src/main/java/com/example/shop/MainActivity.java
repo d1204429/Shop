a9cabce0da42b0e46fb10f17d2ecf4ef5ae7247d
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-08-16 23:25:43 +0800

    實作品牌故事與click here的點擊功能 點擊後顯示品牌故事

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.shop;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,53 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isTextView2Visible = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // 設置邊緣到邊緣佈局
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 獲取 TextView 控件
+        TextView textView = findViewById(R.id.textView);
+        TextView textView2 = findViewById(R.id.textView2);
+        ImageView imageView = findViewById(R.id.icon_image);
+
+        // 設置點擊事件
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isTextView2Visible) {
+                    textView2.setVisibility(View.GONE);
+                    isTextView2Visible = false;
+                } else {
+                    textView2.setVisibility(View.VISIBLE);
+                    isTextView2Visible = true;
+                }
+            }
+
+        });
+
+        // icon_image設置點擊事件
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isTextView2Visible) {
+                    textView2.setVisibility(View.GONE);
+                    isTextView2Visible = false;
+                } else {
+                    textView2.setVisibility(View.VISIBLE);
+                    isTextView2Visible = true;
+                }
+            }
         });
     }
 }
