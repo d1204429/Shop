afb45e27d697cbf7a7236730d85d31fe9d194c38
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-08-18 17:00:26 +0800

    實作icons連結方式

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.os.Handler;
+import android.net.Uri;
+import android.content.Intent;
+import androidx.core.graphics.Insets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private int currentIndex = 0;
     private Handler handler = new Handler();
     private Runnable runnable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
         TextView textView2 = findViewById(R.id.textView2);
         ImageView iconImageView = findViewById(R.id.icon_image);
         imageViewSwitcher = findViewById(R.id.imageView);
+        ImageView internetImageView = findViewById(R.id.internetImageView);
+        ImageView facebookImageView = findViewById(R.id.facebookImageView);
+        ImageView instagramImageView = findViewById(R.id.instagramImageView);
+        ImageView shopeeImageView = findViewById(R.id.shopeeImageView);
+
 
         // 設置點擊事件
         textView.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +87,42 @@ public class MainActivity extends AppCompatActivity {
 
         // 開始圖片輪播
         handler.post(runnable);
+
+        // 打開指定網址
+
+
+        // 設置ImageView的點擊事件，打開相應的網頁
+        internetImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.hohocha.com/?lang=zh-TW"));
+                startActivity(intent);
+            }
+        });
+
+        facebookImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/sunmoonblacktea/?locale=zh_TW"));
+                startActivity(intent);
+            }
+        });
+
+        instagramImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/hohocha/"));
+                startActivity(intent);
+            }
+        });
+
+        shopeeImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://shopee.tw/hohocha?categoryId=100629&entryPoint=ShopByPDP&itemId=6312815046"));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -115,4 +161,18 @@ public class MainActivity extends AppCompatActivity {
         });
         fadeOut.start();
     }
+
+//    private void openWebPage(String url) {
+//        Uri webpage = Uri.parse(url);
+//        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
+//    }
+
+
+
+
+
+
 }
